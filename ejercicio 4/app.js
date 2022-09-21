d6ee9c4ca33765d12b8d9ec7909a49b47ee05247
AIC <104699297+AngelaIC@users.noreply.github.com>
2022-09-21 15:10:41 -0500

    Add files via upload

---
+++ ejercicio 4/app.js
@@ -0,0 +1,6 @@
+function edad(){
+    var fecha = parseInt(prompt("Por favor ingresa tu año de nacimiento"));
+    var res = 2022 - parseInt(fecha);
+    alert("Tienes " + res + " años"); 
+    console.log("Tienes " + res + " años");
+}
